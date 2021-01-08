404cb965a8ebc0757483e7d9a92ea0335f79f3e9
Kumar Saurabh <55243385+KS9004@users.noreply.github.com>
2021-01-08 21:08:19 +0530

    changed the color of camera roll buttion

--- lib/home.dart
+++ lib/home.dart
@@ -123,10 +123,10 @@ class _HomeState extends State<Home> {
                         alignment: Alignment.center,
                         padding: EdgeInsets.symmetric(horizontal: 24,vertical: 17),
                         decoration: BoxDecoration(
-                            color: Color(0xFFE99600),
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(6)
                         ),
-                        child: Text("Camera Roll",style: TextStyle(color: Colors.white),),
+                        child: Text("Camera Roll",style: TextStyle(color: Colors.black),),
                       ),
                     )
                   ],
